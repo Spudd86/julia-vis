df118f74b9ec76fabcafc90d904bc022b702036a
Thomas Jones <thomas.jones@utornto.ca>
2009-01-07 14:16:21 -0500

    fixed bug in bounds on source pixels (off by one)

--- src/map.c
+++ src/map.c
@@ -31,8 +31,8 @@ MAP_FUNC_ATTR void soft_map(uint16_t *out, uint16_t *in, int w, int h, float x0,
 			float y = 2*u*v + y0;
 			float x = u*u - v*v + x0;
 					
-			unsigned int xs = IMIN(IMAX(lrintf(x*w), 0), w);
-			unsigned int ys = IMIN(IMAX(lrintf(y*h), 0), h);
+			unsigned int xs = IMIN(IMAX(lrintf(x*w), 0), w-1);
+			unsigned int ys = IMIN(IMAX(lrintf(y*h), 0), h-1);
 			*out = in[ys*w + xs];
 			out++;
 		}
@@ -70,6 +70,8 @@ MAP_FUNC_ATTR void soft_map_bl(uint16_t *out, uint16_t *in, int w, int h, float 
 
 MAP_FUNC_ATTR void soft_map_interp(uint16_t *restrict out, uint16_t *restrict in, int w, int h, float x0, float y0)
 {
+	//printf("%dx%d\n", w, h);
+	
 	const float ustep = BLOCK_SIZE*2.0f/w, vstep = BLOCK_SIZE*2.0f/h;
 	x0  = x0*0.25f + 0.5f;
 	y0  = y0*0.25f + 0.5f;
@@ -90,16 +92,16 @@ MAP_FUNC_ATTR void soft_map_interp(uint16_t *restrict out, uint16_t *restrict in
 			float x01 = u1*u1 - v0*v0 + x0;
 			float x11 = u1*u1 - v1*v1 + x0;
 			
-			int x0 = IMIN(IMAX(lrintf(x00*w*256), 0), w*256);
-			int y0 = IMIN(IMAX(lrintf(y00*h*256), 0), h*256);
+			int x0 = IMIN(IMAX(lrintf(x00*w*256), 0), (w-1)*256);
+			int y0 = IMIN(IMAX(lrintf(y00*h*256), 0), (h-1)*256);
 			
-			int x1 = IMIN(IMAX(lrintf(x01*w*256), 0), w*256);
-			int y1 = IMIN(IMAX(lrintf(y01*h*256), 0), h*256);
+			int x1 = IMIN(IMAX(lrintf(x01*w*256), 0), (w-1)*256);
+			int y1 = IMIN(IMAX(lrintf(y01*h*256), 0), (h-1)*256);
 			
-			int x0s = (IMIN(IMAX(lrintf(x10*w*256), 0), w*256) - x0)/BLOCK_SIZE;
-			int x1s = (IMIN(IMAX(lrintf(x11*w*256), 0), w*256) - x1)/BLOCK_SIZE;
-			int y0s = (IMIN(IMAX(lrintf(y10*h*256), 0), h*256) - y0)/BLOCK_SIZE;
-			int y1s = (IMIN(IMAX(lrintf(y11*h*256), 0), h*256) - y1)/BLOCK_SIZE;
+			int x0s = (IMIN(IMAX(lrintf(x10*w*256), 0), (w-1)*256) - x0)/BLOCK_SIZE;
+			int x1s = (IMIN(IMAX(lrintf(x11*w*256), 0), (w-1)*256) - x1)/BLOCK_SIZE;
+			int y0s = (IMIN(IMAX(lrintf(y10*h*256), 0), (h-1)*256) - y0)/BLOCK_SIZE;
+			int y1s = (IMIN(IMAX(lrintf(y11*h*256), 0), (h-1)*256) - y1)/BLOCK_SIZE;
 			
 			for(int yt=0; yt<BLOCK_SIZE; yt++, x0+=x0s, y0+=y0s, x1+=x1s, y1+=y1s) {
 				int x = x0;
@@ -112,9 +114,11 @@ MAP_FUNC_ATTR void soft_map_interp(uint16_t *restrict out, uint16_t *restrict in
 
 					int xi1 = xs; 
 					int yi1 = ys*w;
-					int xi2 = IMIN(xi1+1,w);
-					int yi2 = IMIN(yi1+w,h*w);
+					int xi2 = IMIN(xi1+1,w-1);
+					int yi2 = IMIN(yi1+w,h*(w-1));
 					
+					//if(yd+yt>h || xd+xt > w || xd+xt < 0 || yd+yt < 0) 
+					//	{ printf("out of bounds (%d,%d)+(%d,%d) \n",yd,xd,yt,xt); exit(1); }
 					out[(yd+yt)*w+xd+xt] = ((in[yi1 + xi1]*(255 - xf) + in[yi1 + xi2]*xf)*(255-yf) +
 								(in[yi2 + xi1]*(255 - xf) + in[yi2 + xi2]*xf)*yf) >> 16;
 
