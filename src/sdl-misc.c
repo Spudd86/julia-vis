7f8d74f4c490ec6cca994a09cb78d8200f99b624
tomj <tomj@tomato.(none)>
2009-03-13 12:39:51 -0400

    added setting of SDL_NOMOUSE env var so that we work on framebuffer in sdl-misc.c

--- src/sdl-misc.c
+++ src/sdl-misc.c
@@ -16,6 +16,8 @@ static TTF_Font *font = NULL;
 SDL_Surface *sdl_setup(opt_data *opts, int im_size)
 {
 	printf("Initializing SDL.\n");
+	putenv("SDL_NOMOUSE=1");
+	
     if((SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTTHREAD | SDL_INIT_TIMER)==-1)) {
         printf("Could not initialize SDL: %s.\n", SDL_GetError());
         exit(-1);
