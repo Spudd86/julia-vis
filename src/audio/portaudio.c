c38a486a1723c943c1f5e35059b8f16dffa5093b
Thomas Jones <thomas.jones@utoronto.ca>
2009-03-21 23:27:22 -0400

    fix stupidness

--- src/audio/portaudio.c
+++ src/audio/portaudio.c
@@ -27,7 +27,7 @@ int audio_setup_pa()
 	printf("Using PortAudio\n");
 	
 	PaError err = Pa_Initialize();
-	if( err != paNoError ) { printf(  "PortAudio error: %s\n", Pa_GetErrorText(err)); exit(1); }
+	if( err != paNoError ) { fprintf(stderr, "PortAudio error: %s\n", Pa_GetErrorText(err)); exit(1); }
 	
 	const PaDeviceInfo *inf = Pa_GetDeviceInfo(Pa_GetDefaultInputDevice());
 	
@@ -35,18 +35,11 @@ int audio_setup_pa()
     err = Pa_OpenDefaultStream( &stream,
                                 1,          /* 1 input channel */
                                 0,          /* no output */
-                                paFloat32,  /* 32 bit floating point output */
+                                paFloat32,
                                 inf->defaultSampleRate,
-                                1024,        /* frames per buffer, i.e. the number
-                                                   of sample frames that PortAudio will
-                                                   request from the callback. Many apps
-                                                   may want to use
-                                                   paFramesPerBufferUnspecified, which
-                                                   tells PortAudio to pick the best,
-                                                   possibly changing, buffer size.*/
-                                &callback, /* this is your callback function */
-                                NULL ); /*This is a pointer that will be passed to
-                                                   your callback*/
+                                paFramesPerBufferUnspecified,
+                                &callback, 
+                                NULL );
     if( err != paNoError ) goto error;
 	
 	const PaStreamInfo *si = Pa_GetStreamInfo (stream);
@@ -59,10 +52,10 @@ int audio_setup_pa()
 	return 0;
 	
 error:
-	printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+	fprintf(stderr, "PortAudio error: %s\n", Pa_GetErrorText(err));
 	err = Pa_Terminate();
 	if( err != paNoError )
-		printf("Couldn't terminate: PortAudio error: %s\n", Pa_GetErrorText(err));
+		fprintf(stderr, "Couldn't terminate: PortAudio error: %s\n", Pa_GetErrorText(err));
 	return -1;
 }
 
@@ -72,7 +65,8 @@ void audio_stop_pa(void)
 	err = Pa_StopStream( stream ); if( err != paNoError ) goto error;
 	err = Pa_CloseStream( stream ); if( err != paNoError ) goto error;
 	err = Pa_Terminate(); if( err != paNoError ) goto error;
+	return;
 error:
-	printf("Couldn't terminate: PortAudio error: %s\n", Pa_GetErrorText(err));
+	fprintf(stderr, "Couldn't terminate: PortAudio error: %s\n", Pa_GetErrorText(err));
 	exit(1);
 }
