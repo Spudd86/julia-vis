310cb3420363f0db7b1fae02b10485960a9c5224
Thomas Jones <thomas.jones@utoronto.ca>
2009-12-06 18:20:24 -0500

    removed use of SDL_ttf

--- src/sdl-misc.c
+++ src/sdl-misc.c
@@ -1,22 +1,22 @@
 #include "common.h"
 #include <stdio.h>
+#include <string.h>
 
 #include "sdl-misc.h"
 
-#include <SDL_ttf.h>
-
-static TTF_Font *font = NULL;
+//#include <SDL_ttf.h>
+//static TTF_Font *font = NULL;
 
 static SDL_Surface *real_sdl_setup(opt_data *opts, int im_size, int enable_gl);
 
 SDL_Surface *sdl_setup(opt_data *opts, int im_size) {
 	return real_sdl_setup(opts, im_size, 0);
 
-	if(TTF_Init()==-1) {
-		printf("TTF_Init: %s\n", TTF_GetError());
-		exit(2);
-	}
-	font = TTF_OpenFont("font.ttf", 16);
+//	if(TTF_Init()==-1) {
+//		printf("TTF_Init: %s\n", TTF_GetError());
+//		exit(2);
+//	}
+//	font = TTF_OpenFont("font.ttf", 16);
 }
 
 SDL_Surface *sdl_setup_gl(opt_data *opts, int im_size) {
@@ -111,34 +111,55 @@ static SDL_Surface *real_sdl_setup(opt_data *opts, int im_size, int enable_gl)
 	return screen;
 }
 
+SDL_Surface* render_string(const char *str);
 void DrawText(SDL_Surface* screen, const char* text)
 {
-    SDL_Surface *text_surface = TTF_RenderText_Solid(font, text, (SDL_Color){255,255,255,255});
+//    SDL_Surface *text_surface = TTF_RenderText_Solid(font, text, (SDL_Color){255,255,255,255});
+	SDL_Surface *text_surface = render_string(text);
     if (text_surface == NULL) return;
 
 	SDL_BlitSurface(text_surface, NULL, screen, NULL);
 	SDL_FreeSurface(text_surface);
 }
 
-/*
 #include "terminusIBM.h"
 //TODO: write this
-static void draw_string(SDL_Surface *surf, int x, int y, const char *str)
+SDL_Surface* render_string(const char *str)
 {
-	const char *c = str;
-	while(*c) {
-		const uint8_t * restrict src = terminusIBM + 16 * *c;;
-		for(int i=0; i < 16; i++) {
-			uint8_t line = *src++;
-			while(line) {
-				int o = __builtin_ctz(line);
+	int len = strlen(str);
 
+	SDL_Surface *surf = SDL_AllocSurface(SDL_SWSURFACE, len*8, 16, 8, 0, 0, 0, 0);
+    if( surf == NULL ) {
+        return NULL;
+    }
+    SDL_Palette* palette = surf->format->palette;
+    palette->colors[1].r = 255;
+    palette->colors[1].g = 255;
+    palette->colors[1].b = 255;
+    palette->colors[0].r = 0;
+    palette->colors[0].g = 0;
+    palette->colors[0].b = 0;
+    SDL_SetColorKey(surf, SDL_SRCCOLORKEY, 0);
+
+
+	const char *c = str;
+	int x = 0;
+	while(*c) {
+
+		const uint8_t * restrict src = terminusIBM + 16 * *c;;
+		for(int y=0; y < 16; y++) {
+			uint8_t *dst = ((uint8_t*)surf->pixels) + surf->pitch*y + x*8;
+			uint8_t line = *src++;
+			for(int o=0; o < 8; o++) {
+				if(line & (1<<(7-o))) dst[o] = 1;
 			}
 		}
-		c++;
+		c++; x++;
 	}
+
+	return surf;
 }
 
-*/
+
 
 
