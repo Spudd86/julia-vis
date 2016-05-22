a71fb11c60b8ee7e192a315ba7039645034ca61a
Thomas Jones <thomas.jones@utoronto.ca>
2016-05-22 18:33:54 -0400

    softmap: fix bilinear interpolation and make all maps share block interpolation loops

--- src/software/map.c
+++ src/software/map.c
@@ -2,6 +2,57 @@
 #include "mymm.h"
 #include "map.h"
 
+#define BLOCK_SIZE 8
+
+static inline uint16_t bilin_samp(uint16_t *restrict in, int w, int h, float x, float y)
+{
+	int32_t xs = IMIN(IMAX(lrintf(x*w*256), 0), (w-1)*256);
+	int32_t ys = IMIN(IMAX(lrintf(y*h*256), 0), (h-1)*256);
+	int32_t x1 = xs>>8, x2 = x1+1;
+	int32_t y1 = ys>>8, y2 = y1+1;
+	uint_fast32_t xf = xs&0xFF, yf = ys&0xFF;
+
+	// it is critical that this entire calculation be done as uint32s
+	uint32_t v = ((in[y1*w + x1]*(256u - xf) + in[y1*w + x2]*xf)*(256u-yf) +
+			      (in[y2*w + x1]*(256u - xf) + in[y2*w + x2]*xf)*yf) >> 16u;
+
+	return v;
+}
+
+static inline void block_interp_bilin(uint16_t *restrict out, uint16_t *restrict in, int w, int h, int xd, int yd, float x00, float y00, float x01, float y01, float x10, float y10, float x11, float y11)
+{
+	int x0 = IMIN(IMAX(lrintf(x00*w*256), 0), (w-1)*256);
+	int y0 = IMIN(IMAX(lrintf(y00*h*256), 0), (h-1)*256);
+	int x1 = IMIN(IMAX(lrintf(x01*w*256), 0), (w-1)*256);
+	int y1 = IMIN(IMAX(lrintf(y01*h*256), 0), (h-1)*256);
+
+	int x0s = (IMIN(IMAX(lrintf(x10*w*256), 0), (w-1)*256) - x0)/BLOCK_SIZE;
+	int x1s = (IMIN(IMAX(lrintf(x11*w*256), 0), (w-1)*256) - x1)/BLOCK_SIZE;
+	int y0s = (IMIN(IMAX(lrintf(y10*h*256), 0), (h-1)*256) - y0)/BLOCK_SIZE;
+	int y1s = (IMIN(IMAX(lrintf(y11*h*256), 0), (h-1)*256) - y1)/BLOCK_SIZE;
+
+	int y_off = yd * w;
+	for(int yt=0; yt<BLOCK_SIZE; y_off+=w, yt++, x0+=x0s, y0+=y0s, x1+=x1s, y1+=y1s) {
+		int x = x0;
+		int y = y0;
+		int xst = (x1 - x0)/BLOCK_SIZE;
+		int yst = (y1 - y0)/BLOCK_SIZE;
+		for(int xt=0; xt<BLOCK_SIZE; xt++, x+=xst, y+=yst) {
+			int xs=x/256, ys=y/256;
+			uint_fast32_t xf=x&0xFF, yf=y&0xFF;
+
+			int xi1 = xs;
+			int yi1 = ys*w;
+			int xi2 = IMIN(xi1+1,w-1);
+			int yi2 = IMIN(yi1+w,(h-1)*w);
+
+			out[y_off+xd+xt] = ((in[yi1 + xi1]*(256u - xf) + in[yi1 + xi2]*xf)*(256u-yf) +
+						        (in[yi2 + xi1]*(256u - xf) + in[yi2 + xi2]*xf)*yf) >> 16u;
+
+		}
+	}
+}
+
 // also for: (use c99 complex support? get's ugly when expanded...)
 // z = (z^4 + c1)/(z*z + c2)
 //
@@ -30,20 +81,12 @@ MAP_FUNC_ATTR void soft_map(uint16_t *restrict out, uint16_t *restrict in, int w
 			float y = 2*u*v + y0;
 			float x = u*u - v*v + x0;
 
-			int xs = IMIN(IMAX(lrintf(x*w*256), 0), (w-1)*256);
-			int ys = IMIN(IMAX(lrintf(y*h*256), 0), (h-1)*256);
-			int x1 = xs>>8, x2 = x1+1, xf = xs&0xFF;
-			int y1 = ys>>8, y2 = y1+1, yf = ys&0xFF;
-
-			*(out++) = ((in[y1*w + x1]*(0xff - xf) + in[y1*w + x2]*xf)*(0xff-yf) +
-						(in[y2*w + x1]*(0xff - xf) + in[y2*w + x2]*xf)*yf) >> 16;
+			*(out++) = bilin_samp(in, w, h, x, y);
 		}
 	}
 }
 
-#define BLOCK_SIZE 8
-
-MAP_FUNC_ATTR void soft_map_interp(uint16_t *restrict __attribute__((aligned (16))) out, uint16_t *restrict __attribute__ ((aligned (16))) in, int w, int h, const struct point_data *pd)
+MAP_FUNC_ATTR void soft_map_interp(uint16_t *restrict out, uint16_t *restrict in, int w, int h, const struct point_data *pd)
 {
 	const float ustep = BLOCK_SIZE*2.0f/w, vstep = BLOCK_SIZE*2.0f/h;
 	const float cx0 = (pd->p[0]-0.5f)*0.25f + 0.5f, cy0=pd->p[1]*0.25f + 0.5f;
@@ -52,49 +95,22 @@ MAP_FUNC_ATTR void soft_map_interp(uint16_t *restrict __attribute__((aligned (16
 	for(int yd = 0; yd < h; yd+=BLOCK_SIZE) {
 		const float v1 = v0+vstep;
 
-//		const float y00 = -2.0f*v0 + cy0;
-//		const float y10 = -2.0f*v1 + cy0;
-//		const float x00 = 1.0f - v0*v0 + cx0;
-//		const float x10 = 1.0f - v1*v1 + cx0;
+		float y00 = -2.0f*v0 + cy0;
+		float y10 = -2.0f*v1 + cy0;
+		float x00 = 1.0f - v0*v0 + cx0;
+		float x10 = 1.0f - v1*v1 + cx0;
 
-		int x0 = IMIN(IMAX(lrintf((1.0f - v0*v0 + cx0)*w*256), 0), (w-1)*256);
-		int y0 = IMIN(IMAX(lrintf((-2.0f*v0 + cy0)*h*256), 0), (h-1)*256);
-		int x0s = (IMIN(IMAX(lrintf((1.0f - v1*v1 + cx0)*w*256), 0), (w-1)*256) - x0)/BLOCK_SIZE;
-		int y0s = (IMIN(IMAX(lrintf((-2.0f*v1 + cy0)*h*256), 0), (h-1)*256) - y0)/BLOCK_SIZE;
+		float u = ustep - 1.0f;
+		for(int xd = 0; xd < w; xd+=BLOCK_SIZE, u+=ustep) {
+			const float y01 = 2*u*v0 + cy0;
+			const float y11 = 2*u*v1 + cy0;
+			const float x01 = u*u - v0*v0 + cx0;
+			const float x11 = u*u - v1*v1 + cx0;
 
-		float u1 = ustep -1.0f;
-		for(int xd = 0; xd < w; xd+=BLOCK_SIZE, u1+=ustep) {
-			const float y01 = 2*u1*v0 + cy0;
-			const float y11 = 2*u1*v1 + cy0;
-			const float x01 = u1*u1 - v0*v0 + cx0;
-			const float x11 = u1*u1 - v1*v1 + cx0;
+			block_interp_bilin(out, in, w, h, xd, yd, x00, y00, x01, y01, x10, y10, x11, y11);
 
-			const int x1 = IMIN(IMAX(lrintf(x01*w*256), 0), (w-1)*256);
-			const int y1 = IMIN(IMAX(lrintf(y01*h*256), 0), (h-1)*256);
-			const int x1s = (IMIN(IMAX(lrintf(x11*w*256), 0), (w-1)*256) - x1)/BLOCK_SIZE;
-			const int y1s = (IMIN(IMAX(lrintf(y11*h*256), 0), (h-1)*256) - y1)/BLOCK_SIZE;
-
-			const int xsts = (x1s - x0s)/BLOCK_SIZE;
-			const int ysts = (y1s - y0s)/BLOCK_SIZE;
-
-			int xst = (x1 - x0)/BLOCK_SIZE;
-			int yst = (y1 - y0)/BLOCK_SIZE;
-
-			for(int yt=0; yt<BLOCK_SIZE; yt++, x0+=x0s, y0+=y0s, xst += xsts, yst += ysts) {
-				for(int xt=0, x = x0, y = y0; xt<BLOCK_SIZE; xt++, x+=xst, y+=yst) {
-					const int xs=x/256, ys=y/256;
-					const int xf=x&0xFF, yf=y&0xFF;
-					const int xi1 = xs;
-					const int yi1 = ys*w;
-					const int xi2 = IMIN(xi1+1,w-1);
-					const int yi2 = IMIN(yi1+w,(h-1)*w);
-
-					out[(yd+yt)*w+xd+xt] = ((in[yi1 + xi1]*(255 - xf) + in[yi1 + xi2]*xf)*(255-yf) +
-								(in[yi2 + xi1]*(255 - xf) + in[yi2 + xi2]*xf)*yf) >> 16;
-				}
-			}
-			x0 = x1; y0 = y1;
-			x0s = x1s; y0s = y1s;
+			y00 = y01; y10 = y11;
+			x00 = x01; x10 = x11;
 		}
 		v0=v1;
 	}
@@ -104,7 +120,7 @@ MAP_FUNC_ATTR void soft_map_butterfly(uint16_t *restrict out, uint16_t *restrict
 {
 	const float xstep = 2.0f/w, ystep = 2.0f/h;
 	const float sm = sqrtf(2.5f)/2.5f;
-	const float cx0 = pd->p[0]*1.5/2.5, cy0 = pd->p[1]*1.5/2.5;
+	const float cx0 = pd->p[0]*1.5f/2.5f, cy0 = pd->p[1]*1.5f/2.5f;
 
 	for(int yd = 0; yd < h; yd++) {
 		float v = yd*ystep - 1.0f;
@@ -116,24 +132,16 @@ MAP_FUNC_ATTR void soft_map_butterfly(uint16_t *restrict out, uint16_t *restrict
 
 			x = (x+1.0f)*0.5f; y = (y+1.0f)*0.5f;
 
-			int xs = IMIN(IMAX(lrintf(x*w*256), 0), (w-1)*256);
-			int ys = IMIN(IMAX(lrintf(y*h*256), 0), (h-1)*256);
-			int x1 = xs>>8, x2 = x1+1, xf = xs&0xFF;
-			int y1 = ys>>8, y2 = y1+1, yf = ys&0xFF;
-
-			*(out++) = ((in[y1*w + x1]*(0xff - xf) + in[y1*w + x2]*xf)*(0xff-yf) +
-						(in[y2*w + x1]*(0xff - xf) + in[y2*w + x2]*xf)*yf) >> 16;
+			*(out++) = bilin_samp(in, w, h, x, y);
 		}
 	}
 }
 
-
-//TODO: fixme
-MAP_FUNC_ATTR void soft_map_butterfly_interp(uint16_t *restrict __attribute__((aligned (16))) out, uint16_t *restrict __attribute__ ((aligned (16))) in, int w, int h, const struct point_data *pd)
+MAP_FUNC_ATTR void soft_map_butterfly_interp(uint16_t *restrict out, uint16_t *restrict __attribute__ ((aligned (16))) in, int w, int h, const struct point_data *pd)
 {
 	const float ustep = 2.0f/w, vstep = 2.0f/h;
 	const float sm = sqrtf(2.5f)/2.5f;
-	const float cx0 = pd->p[0]*1.5/2.5, cy0 = pd->p[1]*1.5/2.5;
+	const float cx0 = pd->p[0]*1.5f/2.5f, cy0 = pd->p[1]*1.5f/2.5f;
 
 	for(int yd = 0; yd < h; yd+=BLOCK_SIZE) {
 		float v0 = yd*vstep - 1.0f;
@@ -156,47 +164,8 @@ MAP_FUNC_ATTR void soft_map_butterfly_interp(uint16_t *restrict __attribute__((a
 			y01 = (y01+1.0f)*0.5f; y11 = (y11+1.0f)*0.5f;
 			x01 = (x01+1.0f)*0.5f; x11 = (x11+1.0f)*0.5f;
 
-//			int x0 = IMIN(IMAX(lrintf(x00*w*256), 0), (w-1)*256);
-//			int y0 = IMIN(IMAX(lrintf(y00*h*256), 0), (h-1)*256);
-//
-//			int x1 = IMIN(IMAX(lrintf(x01*w*256), 0), (w-1)*256);
-//			int y1 = IMIN(IMAX(lrintf(y01*h*256), 0), (h-1)*256);
-//
-//			int x0s = (IMIN(IMAX(lrintf(x10*w*256), 0), (w-1)*256) - x0)/BLOCK_SIZE;
-//			int x1s = (IMIN(IMAX(lrintf(x11*w*256), 0), (w-1)*256) - x1)/BLOCK_SIZE;
-//			int y0s = (IMIN(IMAX(lrintf(y10*h*256), 0), (h-1)*256) - y0)/BLOCK_SIZE;
-//			int y1s = (IMIN(IMAX(lrintf(y11*h*256), 0), (h-1)*256) - y1)/BLOCK_SIZE;
+			block_interp_bilin(out, in, w, h, xd, yd, x00, y00, x01, y01, x10, y10, x11, y11);
 
-			float x0 = x00, y0 = y00, x1 = x01, y1 = y01;
-			float x0s = (x10 - x0)/BLOCK_SIZE, x1s  = (x11 - x1)/BLOCK_SIZE;
-			float y0s = (y10 - y0)/BLOCK_SIZE, y1s  = (y11 - y1)/BLOCK_SIZE;
-
-			for(int yt=0; yt<BLOCK_SIZE; yt++, x0+=x0s, y0+=y0s, x1+=x1s, y1+=y1s) {
-//				int x = x0, y = y0; int xst = (x1 - x0)/BLOCK_SIZE, yst = (y1 - y0)/BLOCK_SIZE;
-				float x = x0, y = y0;
-				float xst = (x1 - x0)/BLOCK_SIZE, yst = (y1 - y0)/BLOCK_SIZE;
-				for(int xt=0; xt<BLOCK_SIZE; xt++, x+=xst, y+=yst) {
-
-					int xs = IMIN(IMAX(lrintf(x*w*256), 0), (w-1)*256);
-					int ys = IMIN(IMAX(lrintf(y*h*256), 0), (h-1)*256);
-					int x1 = xs>>8, x2 = x1+1, xf = xs&0xFF;
-					int y1 = ys>>8, y2 = y1+1, yf = ys&0xFF;
-					*(out++) = ((in[y1*w + x1]*(0xff - xf) + in[y1*w + x2]*xf)*(0xff-yf) +
-								(in[y2*w + x1]*(0xff - xf) + in[y2*w + x2]*xf)*yf) >> 16;
-
-//					int xs=x/256, ys=y/256;
-//					int xf=x&0xFF, yf=y&0xFF;
-//
-//					int xi1 = xs;
-//					int yi1 = ys*w;
-//					int xi2 = IMIN(xi1+1,w-1);
-//					int yi2 = IMIN(yi1+w,(h-1)*w);
-//
-//					out[(yd+yt)*w+xd+xt] = ((in[yi1 + xi1]*(255 - xf) + in[yi1 + xi2]*xf)*(255-yf) +
-//								(in[yi2 + xi1]*(255 - xf) + in[yi2 + xi2]*xf)*yf) >> 16;
-
-				}
-			}
 			y00 = y01; y10 = y11;
 			x00 = x01; x10 = x11;
 		}
@@ -224,13 +193,7 @@ MAP_FUNC_ATTR void soft_map_rational(uint16_t *restrict out, uint16_t *restrict 
 
 			x = (x+1.0f)*0.5f; y = (y+1.0f)*0.5f;
 
-			int xs = IMIN(IMAX(lrintf(x*w*256), 0), (w-1)*256);
-			int ys = IMIN(IMAX(lrintf(y*h*256), 0), (h-1)*256);
-			int x1 = xs>>8, x2 = x1+1, xf = xs&0xFF;
-			int y1 = ys>>8, y2 = y1+1, yf = ys&0xFF;
-
-			*(out++) = ((in[y1*w + x1]*(0xff - xf) + in[y1*w + x2]*xf)*(0xff-yf) +
-						(in[y2*w + x1]*(0xff - xf) + in[y2*w + x2]*xf)*yf) >> 16;
+			*(out++) = bilin_samp(in, w, h, x, y);
 		}
 	}
 }
@@ -283,59 +246,11 @@ MAP_FUNC_ATTR void soft_map_rational_interp(uint16_t *restrict out, uint16_t *re
 			float y11 = (y+1.0f)*0.5f;
 			float x11 = (x+1.0f)*0.5f;
 
-			int x0 = IMIN(IMAX(lrintf(x00*w*256), 0), (w-1)*256);
-			int y0 = IMIN(IMAX(lrintf(y00*h*256), 0), (h-1)*256);
-			int x1 = IMIN(IMAX(lrintf(x01*w*256), 0), (w-1)*256);
-			int y1 = IMIN(IMAX(lrintf(y01*h*256), 0), (h-1)*256);
+			block_interp_bilin(out, in, w, h, xd, yd, x00, y00, x01, y01, x10, y10, x11, y11);
 
-			int x0s = (IMIN(IMAX(lrintf(x10*w*256), 0), (w-1)*256) - x0)/BLOCK_SIZE;
-			int x1s = (IMIN(IMAX(lrintf(x11*w*256), 0), (w-1)*256) - x1)/BLOCK_SIZE;
-			int y0s = (IMIN(IMAX(lrintf(y10*h*256), 0), (h-1)*256) - y0)/BLOCK_SIZE;
-			int y1s = (IMIN(IMAX(lrintf(y11*h*256), 0), (h-1)*256) - y1)/BLOCK_SIZE;
-
-			for(int yt=0; yt<BLOCK_SIZE; yt++, x0+=x0s, y0+=y0s, x1+=x1s, y1+=y1s) {
-				int x = x0;
-				int y = y0;
-				int xst = (x1 - x0)/BLOCK_SIZE;
-				int yst = (y1 - y0)/BLOCK_SIZE;
-				for(int xt=0; xt<BLOCK_SIZE; xt++, x+=xst, y+=yst) {
-					int xs=x/256, ys=y/256;
-					int xf=x&0xFF, yf=y&0xFF;
-
-					int xi1 = xs;
-					int yi1 = ys*w;
-					int xi2 = IMIN(xi1+1,w-1);
-					int yi2 = IMIN(yi1+w,(h-1)*w);
-
-					out[(yd+yt)*w+xd+xt] = ((in[yi1 + xi1]*(255 - xf) + in[yi1 + xi2]*xf)*(255-yf) +
-								(in[yi2 + xi1]*(255 - xf) + in[yi2 + xi2]*xf)*yf) >> 16;
-
-				}
-			}
 			y00 = y01; y10 = y11;
 			x00 = x01; x10 = x11;
 		}
 		v0=v1;
 	}
 }
-
-//~ MAP_FUNC_ATTR void soft_map_rational(uint16_t *restrict out, uint16_t *restrict in, int w, int h, float cx0, float cy0, float cx1, float cy1 )
-//~ {
-	//~ float complex c1 = cx0 + cy0*I; float complex c2 = cx1 + cy1*I;
-	//~ float xstep = 2.0f/w, ystep = 2.0f/h;
-	//~ for(int yd = 0; yd < h; yd++) {
-		//~ float v = 3*(yd*ystep - 1.0f);
-		//~ for(int xd = 0; xd < w; xd++) {
-			//~ float complex z = 3*(xd*xstep -1.0f + v*I);
-			//~ float complex zsqr = z*z;
-
-			//~ z = ((zsqr*zsqr + c1)/(zsqr + c2))/3.0f;
-
-			//~ unsigned int xs = IMIN(IMAX(lrintf(crealf(z)*w), 0), w-1);
-			//~ unsigned int ys = IMIN(IMAX(lrintf(cimagf(z)*h), 0), h-1);
-			//~ *out = in[ys*w + xs];
-			//~ out++;
-		//~ }
-	//~ }
-//~ }
-
