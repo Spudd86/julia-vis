6f6df27a4c5a59cf318c2a5b62446c0e59636a4d
Thomas Jones <thomas.jones@utoronto.ca>
2016-08-08 23:27:51 -0400

    x86: optimize pallet_blit32_sse2()

--- src/software/x86/sse2.c
+++ src/software/x86/sse2.c
@@ -59,129 +59,18 @@ void maxblend_sse2(void *restrict dest, const void *restrict src, int w, int h)
 	}
 }
 
-// this is exactly what the gcc version of the intrinsic _mm_loadl_epi64 does
-// only we don't cast away const and restrict
-//TODO: work out weather or not _mm_loadu_si128 works just as well
-#define my_load_epi64(p) (_mm_set_epi64((__m64)0LL, *(const __m64 *restrict)(p)))
-#define pb32_load_v(s) (_mm_setr_epi16(*((s)+0), *((s)+0), *((s)+0), *((s)+0), *((s)+1), *((s)+1), *((s)+1), *((s)+1)))
-//#define pb32_load_v(s) (_mm_set1_epi32(*(const uint32_t *)(s)))
+//#define pb32_load_v(s) (_mm_setr_epi16(*((s)+0), *((s)+0), *((s)+0), *((s)+0), *((s)+1), *((s)+1), *((s)+1), *((s)+1)))
+static inline __attribute__((__gnu_inline__, __always_inline__, __artificial__))
+__m128i pb32_load_v(const uint16_t *restrict s) {
+	// yes this REALLY is faster than calling _mm_setr_epi16
+	__m128i r = (__m128i)(__v4si){0, 0, 0, *(const uint32_t *)(s)};
+	r = _mm_shuffle_epi32(r, 0);
+	r = _mm_shufflehi_epi16(r, _MM_SHUFFLE(1,1,1,1));
+	r = _mm_shufflelo_epi16(r, 0);
+	return r;
+}
 
 #if 1
-__attribute__((always_inline,hot))
-static inline void blit32_1(uint32_t *restrict d, const uint16_t *restrict s, const uint32_t *restrict pal)
-{
-	//TODO: make a version of this that works with icc
-	// doing this because we want to be absolutely sure the compiler can constant fold and merge constants
-	// after inlining
-	static const __m128i zero = (const __m128i){0ll, 0ll};
-	static const __m128i mask = (const __m128i){0x00ff00ff00ff00ffll, 0x00ff00ff00ff00ffll};
-	static const __m128i sub  = (const __m128i){0x0100010001000100ll, 0x0100010001000100ll};
-
-	__m128i col1, col2, v1, v1s;
-
-	col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[0]/256u)));
-	col1 = _mm_unpacklo_epi32(col1, zero);
-	col2 = col1;
-	col1 = _mm_unpacklo_epi8(col1, zero);
-	col2 = _mm_unpackhi_epi8(col2, zero);
-
-	v1   = _mm_set1_epi16(*(s + 0));
-	v1   = _mm_and_si128(v1, mask);
-	col2 = _mm_mullo_epi16(col2, v1);
-	v1s  = _mm_sub_epi16(sub, v1);
-	col1 = _mm_mullo_epi16(col1, v1s);
-	col1 = _mm_add_epi16(col1, col2);
-	col1 = _mm_srli_epi16(col1, 8);
-
-	col1 = _mm_packus_epi16(col1, zero);
-	_mm_stream_si32((int *)d, _mm_cvtsi128_si32(col1));
-}
-
-__attribute__((always_inline,hot))
-static inline void blit32_2(uint32_t *restrict d, const uint16_t *restrict s, const uint32_t *restrict pal)
-{
-	//TODO: make a version of this that works with icc
-	// doing this because we want to be absolutely sure the compiler can constant fold and merge constants
-	// after inlining
-	static const __m128i zero = (const __m128i){0ll, 0ll};
-	static const __m128i mask = (const __m128i){0x00ff00ff00ff00ffll, 0x00ff00ff00ff00ffll};
-	static const __m128i sub  = (const __m128i){0x0100010001000100ll, 0x0100010001000100ll};
-
-	__m128i col1, col2, v1, v1s;
-	col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[0]/256u)));
-	col2 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[1]/256u)));
-	col1 = _mm_unpacklo_epi32(col1, col2);
-	col2 = col1;
-	col1 = _mm_unpacklo_epi8(col1, zero);
-	col2 = _mm_unpackhi_epi8(col2, zero);
-
-	v1   = pb32_load_v(s+0);
-	v1   = _mm_and_si128(v1, mask);
-	col2 = _mm_mullo_epi16(col2, v1);
-	v1s  = _mm_sub_epi16(sub, v1);
-	col1 = _mm_mullo_epi16(col1, v1s);
-	col1 = _mm_add_epi16(col1, col2);
-	col1 = _mm_srli_epi16(col1, 8);
-
-	col1 = _mm_packus_epi16(col1, zero);
-#if __x86_64__  //TODO: test which of these is fastest on x86_64
-	_mm_stream_si64((int64_t *)d, _mm_cvtsi128_si64(col1)); // x86_64 only
-#else
-	_mm_stream_pi((__m64 *)d, _mm_movepi64_pi64(col1));
-#endif
-}
-
-__attribute__((always_inline,hot))
-static inline void blit32_4(uint32_t *restrict d, const uint16_t *restrict s, const uint32_t *restrict pal)
-{
-	//TODO: make a version of this that works with icc
-	// doing this because we want to be absolutely sure the compiler can constant fold and merge constants
-	// after inlining
-	static const __m128i zero = (const __m128i){0ll, 0ll};
-	static const __m128i mask = (const __m128i){0x00ff00ff00ff00ffll, 0x00ff00ff00ff00ffll};
-	static const __m128i sub  = (const __m128i){0x0100010001000100ll, 0x0100010001000100ll};
-
-	__m128i col1, col2, col3, col4, v1, v2, v1s, v2s;
-	col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[0]/256u)));
-	col2 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[1]/256u)));
-	col1 = _mm_unpacklo_epi32(col1, col2);
-	col2 = col1;
-	col1 = _mm_unpacklo_epi8(col1, zero);
-	col2 = _mm_unpackhi_epi8(col2, zero);
-
-	v1   = pb32_load_v(s+0);
-	v1   = _mm_and_si128(v1, mask);
-	col2 = _mm_mullo_epi16(col2, v1);
-	v1s  = _mm_sub_epi16(sub, v1);
-	col1 = _mm_mullo_epi16(col1, v1s);
-	col1 = _mm_add_epi16(col1, col2);
-	col1 = _mm_srli_epi16(col1, 8);
-
-	col1 = _mm_packus_epi16(col1, zero);
-
-
-	col3 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[2]/256u)));
-	col4 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[3]/256u)));
-
-	col3 = _mm_unpacklo_epi32(col3, col4);
-	col4 = col3;
-	col3 = _mm_unpacklo_epi8(col3, zero);
-	col4 = _mm_unpackhi_epi8(col4, zero);
-
-	v2   = pb32_load_v(s+2);
-	v2   = _mm_and_si128(v2, mask);
-	col4 = _mm_mullo_epi16(col4, v2);
-	v2s  = _mm_sub_epi16(sub, v2);
-	col3 = _mm_mullo_epi16(col3, v2s);
-	col3 = _mm_add_epi16(col3, col4);
-	col3 = _mm_srli_epi16(col3, 8);
-
-	col3 = _mm_packus_epi16(col3, zero);
-	_mm_stream_si128((__m128i *)(d + 0), _mm_unpacklo_epi64(col1, col3));
-}
-
-//TODO: test if going up to a full 16 pixel at a time loop is faster, could be due to
-// that being a cache line size chuck
 __attribute__((hot))
 void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
                         const uint16_t *restrict src, unsigned int src_stride,
@@ -189,13 +78,14 @@ void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
                         const uint32_t *restrict pal)
 {
 	//FIXME: add src_stride back in
-	static const __m128i zero = (const __m128i){0ll, 0ll}; // _mm_setzero_si128();
-	static const __m128i mask = (const __m128i){0x00ff00ff00ff00ffll, 0x00ff00ff00ff00ffll}; // _mm_set1_epi16(0xff);
-	static const __m128i sub  = (const __m128i){0x0100010001000100ll, 0x0100010001000100ll}; // _mm_set1_epi16(256);
+	static const __m128i zero  = (const __m128i){0ll, 0ll}; // _mm_setzero_si128();
+	static const __m128i mask  = (const __m128i){0x00ff00ff00ff00ffll, 0x00ff00ff00ff00ffll}; // _mm_set1_epi16(0xff);
+	static const __m128i sub   = (const __m128i){0x0100010001000100ll, 0x0100010001000100ll}; // _mm_set1_epi16(256);
+	static const __m128i mvcmp = (const __m128i){0x0000000000000001ll, 0x0000000200000003ll}; // _mm_setr_epi32(0, 1, 2, 3)
 
 
 	for(size_t y = 0; y < h; y++) {
-		const uint16_t *restrict s = src + y*w; // for some reason actually using src_sride is a lot slower (like 10FPS)
+		const uint16_t *restrict s = src + y*w; // for some reason actually using src_sride is a lot slower (like 10FPS) maybe because it's doing a transformation of some kind...
 		_mm_prefetch(s, _MM_HINT_NTA);
 
 		uint32_t *restrict d = (uint32_t *restrict)(dest + y*dst_stride);
@@ -204,44 +94,21 @@ void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
 		// need to align our destination address so we can use non-temporal writes
 		// assume that the pixels are at least aligned to 4 bytes
 		uintptr_t dp = (uintptr_t)d;
-		switch(4 - ((dp%16)/4)) {
-			case 2:
-				blit32_2(d, s, pal);
-				s+=2;
-				d+=2;
-				x+=2;
-			break;
-			case 3:
-				blit32_2(d, s, pal);
-				s+=2;
-				d+=2;
-				x+=2;
-			//fall through
-			case 1:
-				blit32_1(d, s, pal);
-				s++;
-				d++;
-				x++;
-			case 0:
-			case 4:
-				break;
-			default:
-				unreachable();// can't happen
-		}
-
-		for(; x + 8 < w; x+=8, s+=8, d+=8) {
-			_mm_prefetch(s + 8, _MM_HINT_NTA);
+		dp = 4 - ((dp%16)/4);
+		if(dp) {
+			const __m128i move_mask = _mm_cmplt_epi32(mvcmp, _mm_set1_epi32(dp));
 			__m128i col1, col2, col3, col4, v1, v2, v1s, v2s;
 
 			col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[0]/256u)));
 			col2 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[1]/256u)));
+			v1   = pb32_load_v(s+0);
+			v1   = _mm_and_si128(v1, mask);
+
 			col1 = _mm_unpacklo_epi32(col1, col2);
 			col2 = col1;
 			col1 = _mm_unpacklo_epi8(col1, zero);
 			col2 = _mm_unpackhi_epi8(col2, zero);
 
-			v1   = pb32_load_v(s+0);
-			v1   = _mm_and_si128(v1, mask);
 			col2 = _mm_mullo_epi16(col2, v1);
 			v1s  = _mm_sub_epi16(sub, v1);
 			col1 = _mm_mullo_epi16(col1, v1s);
@@ -253,14 +120,15 @@ void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
 
 			col3 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[2]/256u)));
 			col4 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[3]/256u)));
+			v2   = pb32_load_v(s+2);
+			v2   = _mm_and_si128(v2, mask);
 
 			col3 = _mm_unpacklo_epi32(col3, col4);
 			col4 = col3;
 			col3 = _mm_unpacklo_epi8(col3, zero);
 			col4 = _mm_unpackhi_epi8(col4, zero);
 
-			v2   = pb32_load_v(s+2);
-			v2   = _mm_and_si128(v2, mask);
+			
 			col4 = _mm_mullo_epi16(col4, v2);
 			v2s  = _mm_sub_epi16(sub, v2);
 			col3 = _mm_mullo_epi16(col3, v2s);
@@ -269,18 +137,72 @@ void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
 
 			col3 = _mm_packus_epi16(col3, zero);
 
-			_mm_stream_si128((__m128i *)(d + 0), _mm_unpacklo_epi64(col1, col3));
-			//_mm_storeu_si128((__m128i *restrict)(d + 0), _mm_unpacklo_epi64(col1, col3));
+			col1 = _mm_unpacklo_epi64(col1, col3);
+			_mm_maskmoveu_si128(col1, move_mask, (char *restrict)(d + 0));
+			x+=dp;
+			d+=dp;
+			s+=dp;
+		}
 
-			col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[4]/256u)));
-			col2 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[5]/256u)));
+		// want to go 8 pixels at a time because that way our
+		// prefetches line up nicely, the prefetch is guaranteed to do 32 bytes
+		// but might do more. We'll just assume the minimum that we get decent
+		// performance even on old systems with smaller cache lines
+		for(; x + 8 < w; x+=8, s+=8, d+=8) {
+			_mm_prefetch(s + 8, _MM_HINT_NTA);
+			__m128i col1, col2, col3, col4, v1, v2, v1s, v2s;
+
+			col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[0]/256u)));
+			col2 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[1]/256u)));
+			v1   = pb32_load_v(s+0);
+			v1   = _mm_and_si128(v1, mask);
+
 			col1 = _mm_unpacklo_epi32(col1, col2);
 			col2 = col1;
 			col1 = _mm_unpacklo_epi8(col1, zero);
 			col2 = _mm_unpackhi_epi8(col2, zero);
 
+			col2 = _mm_mullo_epi16(col2, v1);
+			v1s  = _mm_sub_epi16(sub, v1);
+			col1 = _mm_mullo_epi16(col1, v1s);
+			col1 = _mm_add_epi16(col1, col2);
+			col1 = _mm_srli_epi16(col1, 8);
+
+			col1 = _mm_packus_epi16(col1, zero);
+
+
+			col3 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[2]/256u)));
+			col4 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[3]/256u)));
+			v2   = pb32_load_v(s+2);
+			v2   = _mm_and_si128(v2, mask);
+
+			col3 = _mm_unpacklo_epi32(col3, col4);
+			col4 = col3;
+			col3 = _mm_unpacklo_epi8(col3, zero);
+			col4 = _mm_unpackhi_epi8(col4, zero);
+
+			col4 = _mm_mullo_epi16(col4, v2);
+			v2s  = _mm_sub_epi16(sub, v2);
+			col3 = _mm_mullo_epi16(col3, v2s);
+			col3 = _mm_add_epi16(col3, col4);
+			col3 = _mm_srli_epi16(col3, 8);
+
+			col3 = _mm_packus_epi16(col3, zero);
+
+			col1 = _mm_unpacklo_epi64(col1, col3);
+			_mm_stream_si128((__m128i *)(d + 0), col1);
+			//_mm_storeu_si128((__m128i *restrict)(d + 0), col1);
+
+			col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[4]/256u)));
+			col2 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[5]/256u)));
 			v1   = pb32_load_v(s+4);
 			v1   = _mm_and_si128(v1, mask);
+
+			col1 = _mm_unpacklo_epi32(col1, col2);
+			col2 = col1;
+			col1 = _mm_unpacklo_epi8(col1, zero);
+			col2 = _mm_unpackhi_epi8(col2, zero);
+
 			col2 = _mm_mullo_epi16(col2, v1);
 			v1s  = _mm_sub_epi16(sub, v1);
 			col1 = _mm_mullo_epi16(col1, v1s);
@@ -292,14 +214,14 @@ void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
 
 			col3 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[6]/256u)));
 			col4 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[7]/256u)));
+			v2   = pb32_load_v(s+6);
+			v2   = _mm_and_si128(v2, mask);
 
 			col3 = _mm_unpacklo_epi32(col3, col4);
 			col4 = col3;
 			col3 = _mm_unpacklo_epi8(col3, zero);
 			col4 = _mm_unpackhi_epi8(col4, zero);
 
-			v2   = pb32_load_v(s+6);
-			v2   = _mm_and_si128(v2, mask);
 			col4 = _mm_mullo_epi16(col4, v2);
 			v2s  = _mm_sub_epi16(sub, v2);
 			col3 = _mm_mullo_epi16(col3, v2s);
@@ -308,38 +230,110 @@ void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
 
 			col3 = _mm_packus_epi16(col3, zero);
 
-			_mm_stream_si128((__m128i *)(d + 4), _mm_unpacklo_epi64(col1, col3));
-			//_mm_storeu_si128((__m128i *restrict)(d + 4), _mm_unpacklo_epi64(col1, col3));
+			col1 = _mm_unpacklo_epi64(col1, col3);
+			_mm_stream_si128((__m128i *)(d + 4), col1);
+			//_mm_storeu_si128((__m128i *restrict)(d + 4), col1);
+
+			// would it be faster to just do the maskmove here and not bother with
+			// the stuff outside the loop?
+			// It'd be a lot less code but we'd be adding
+			// at least two extra instructions inside the loop to do the add and compare
+			// plus extra register use, which on 32 bit means we need to spill even more
+			// than we already do.
 		}
 
 		// we'll never have less than 4 pixels to do at the end because w is
 		// always divisible by 16 (and therefore 8, so since we only did 0 - 3
 		// pixels to force alignment and the loop does 8 pixels per iteration
 		// we end up with 4 - 8 pixels that need doing at loop exit)
-		blit32_4(d, s, pal);
+		// so only the second set of four needs to use a maskmove
 
-		x+=4;
-		s+=4;
-		d+=4;
+		__m128i col1, col2, col3, col4, v1, v2, v1s, v2s;
 
-		switch(w-x) {
-			case 4:
-				blit32_4(d, s, pal);
-			break;
-			case 2:
-				blit32_2(d, s, pal);
-			break;
-			case 3:
-				blit32_2(d, s, pal);
-				s+=2;
-				d+=2;
-			//fall through
-			case 1:
-				blit32_1(d, s, pal);
-			case 0:
-				break;
-			default:
-				unreachable(); // can't happen
+		col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[0]/256u)));
+		col2 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[1]/256u)));
+		v1   = pb32_load_v(s+0);
+		v1   = _mm_and_si128(v1, mask);
+
+		col1 = _mm_unpacklo_epi32(col1, col2);
+		col2 = col1;
+		col1 = _mm_unpacklo_epi8(col1, zero);
+		col2 = _mm_unpackhi_epi8(col2, zero);
+
+		col2 = _mm_mullo_epi16(col2, v1);
+		v1s  = _mm_sub_epi16(sub, v1);
+		col1 = _mm_mullo_epi16(col1, v1s);
+		col1 = _mm_add_epi16(col1, col2);
+		col1 = _mm_srli_epi16(col1, 8);
+
+		col1 = _mm_packus_epi16(col1, zero);
+
+
+		col3 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[2]/256u)));
+		col4 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[3]/256u)));
+		v2   = pb32_load_v(s+2);
+		v2   = _mm_and_si128(v2, mask);
+
+		col3 = _mm_unpacklo_epi32(col3, col4);
+		col4 = col3;
+		col3 = _mm_unpacklo_epi8(col3, zero);
+		col4 = _mm_unpackhi_epi8(col4, zero);
+
+		col4 = _mm_mullo_epi16(col4, v2);
+		v2s  = _mm_sub_epi16(sub, v2);
+		col3 = _mm_mullo_epi16(col3, v2s);
+		col3 = _mm_add_epi16(col3, col4);
+		col3 = _mm_srli_epi16(col3, 8);
+
+		col3 = _mm_packus_epi16(col3, zero);
+
+		col1 = _mm_unpacklo_epi64(col1, col3);
+		_mm_stream_si128((__m128i *)(d + 0), col1);
+
+		if(w-(x+4)) { // should be the same as 4-dp
+		//if(4-dp) {
+			const __m128i move_mask = _mm_cmplt_epi32(mvcmp, _mm_set1_epi32(w-(x+4)));
+			//const __m128i move_mask = _mm_cmplt_epi32(_mm_setr_epi32(0, 1, 2, 3), _mm_set1_epi32(4-dp));
+
+			col1 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[4]/256u)));
+			col2 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[5]/256u)));
+			v1   = pb32_load_v(s+4);
+			v1   = _mm_and_si128(v1, mask);
+
+			col1 = _mm_unpacklo_epi32(col1, col2);
+			col2 = col1;
+			col1 = _mm_unpacklo_epi8(col1, zero);
+			col2 = _mm_unpackhi_epi8(col2, zero);
+
+			col2 = _mm_mullo_epi16(col2, v1);
+			v1s  = _mm_sub_epi16(sub, v1);
+			col1 = _mm_mullo_epi16(col1, v1s);
+			col1 = _mm_add_epi16(col1, col2);
+			col1 = _mm_srli_epi16(col1, 8);
+
+			col1 = _mm_packus_epi16(col1, zero);
+
+
+			col3 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[6]/256u)));
+			col4 = _mm_loadl_epi64((const __m128i *restrict)(pal+(s[7]/256u)));
+			v2   = pb32_load_v(s+6);
+			v2   = _mm_and_si128(v2, mask);
+
+			col3 = _mm_unpacklo_epi32(col3, col4);
+			col4 = col3;
+			col3 = _mm_unpacklo_epi8(col3, zero);
+			col4 = _mm_unpackhi_epi8(col4, zero);
+
+			col4 = _mm_mullo_epi16(col4, v2);
+			v2s  = _mm_sub_epi16(sub, v2);
+			col3 = _mm_mullo_epi16(col3, v2s);
+			col3 = _mm_add_epi16(col3, col4);
+			col3 = _mm_srli_epi16(col3, 8);
+
+			col3 = _mm_packus_epi16(col3, zero);
+
+			col1 = _mm_unpacklo_epi64(col1, col3);
+			_mm_maskmoveu_si128(col1, move_mask, (char *restrict)(d + 4));
 		}
 	}
 #if !__x86_64__
@@ -348,6 +342,10 @@ void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
 	_mm_sfence(); // needed because of the non-temporal stores.
 }
 #else
+
+// *******************************************************************************************************************
+// **************************************** Simple unaligned accesses version ****************************************
+// mostly here for reference and comparison.
 __attribute__((hot))
 void pallet_blit32_sse2(uint8_t *restrict dest, unsigned int dst_stride,
                         const uint16_t *restrict src, unsigned int src_stride,
