2c4178a654ff9dba33141949309fee5b562b47c4
Thomas Jones <thomas.jones@utoronto.ca>
2021-09-09 16:34:55 -0400

    add Gstreamer testing scripts

---
+++ encode_with_gst.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#FILENAME=megaman.mp3
+#FILENAME=232307_Euphoria_Edit.mp3
+
+#OUTFILE=test
+
+#FILENAME=../238913__edtijo__hejda-depressive-guitar.wav; OUTFILE=guitar
+
+FILENAME=../379875_The_time_portal.mp3; OUTFILE="/media/thomasjones/My Passport/the_time_portal"
+
+#GDB='gdb --args'
+
+#DEBUG_ARGS="--gst-debug=julia_vis:9 --gst-debug=baseaudiovisualizer-julia:9"
+#DEBUG_ARGS="--gst-debug=julia_vis:9"
+
+#TIME_OVERLAY1="timeoverlay time-mode=buffer-time ypos=0 ! timeoverlay time-mode=stream-time ypos=0 deltay=32"
+#TIME_OVERLAY2="timeoverlay time-mode=stream-time ypos=0 deltay=64 color=0xffffff00"
+TIME_OVERLAY1="identity"
+TIME_OVERLAY2="identity"
+
+#VIS_PLUGIN="julia_vis map-func=rational"
+VIS_PLUGIN="julia_vis map-func=normal"
+#VIS_PLUGIN="julia_vis"
+
+#VID_FORMAT="video/x-raw,width=384,height=384,framerate=30/1"
+#VID_FORMAT="video/x-raw,width=1024,height=1024,framerate=60/1"
+VID_FORMAT="video/x-raw,width=8192,height=8192,framerate=60/1"
+#VID_FORMAT="video/x-raw,width=16384,height=16384,framerate=60/1"
+
+#VID_POST_PROC="identity"
+#1080p
+#VID_POST_PROC="videoscale n-threads=8 gamma-decode=1 method=lanczos dither=true ! video/x-raw,width=1080,height=1080"
+#2k/1440p video
+#VID_POST_PROC="videoscale n-threads=8 gamma-decode=1 method=lanczos dither=true ! video/x-raw,width=1440,height=1440"
+#4k video
+VID_POST_PROC="videoscale n-threads=8 gamma-decode=1 method=lanczos dither=true ! video/x-raw,width=2160,height=2160"
+#8k video
+#VID_POST_PROC="videoscale n-threads=8 gamma-decode=1 method=lanczos dither=true ! video/x-raw,width=4320,height=4320"
+
+#ENCODER="x264enc vbv-buf-capacity=8388608 bitrate=40000 pass=5 speed-preset=veryslow quantizer=14 qp-min=5"
+#1Mbit/s max
+ENCODER="x264enc vbv-buf-capacity=8388608 bitrate=1048576 pass=5 speed-preset=veryslow quantizer=12 qp-min=1"
+#ENCODER="av1enc target-bitrate=40000"
+
+GST_PLUGIN_PATH=bin/.libs $GDB gst-launch-1.0 $DEBUG_ARGS \
+matroskamux name=mux ! filesink location="$OUTFILE.mkv" \
+streamsynchronizer name=s \
+filesrc location="$FILENAME" ! decodebin ! progressreport update-freq=1 ! audioconvert ! tee name=t \
+t.src_0 ! queue ! $VIS_PLUGIN ! $VID_FORMAT ! $TIME_OVERLAY1 ! s.sink_0 \
+t.src_1 ! queue max-size-bytes=0 max-size-buffers=0 max-size-time=10000000000 ! audioconvert ! audioresample ! s.sink_1 \
+s.src_0 ! $VID_POST_PROC ! videoconvert ! $TIME_OVERLAY2 ! $ENCODER ! mux. \
+s.src_1 ! audio/x-raw,format=S16LE,channels=2 ! mux. 
---
+++ test-gst-plugin.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+#TODO: command line arguments
+
+
+#GDB='gdb --args'
+
+#DEBUG_ARGS="--gst-debug=julia_vis:9 --gst-debug=baseaudiovisualizer-julia:9"
+#DEBUG_ARGS="--gst-debug=julia_vis:9"
+
+#TIME_OVERLAY1="timeoverlay time-mode=buffer-time ypos=0 ! timeoverlay time-mode=stream-time ypos=0 deltay=32"
+#TIME_OVERLAY2="timeoverlay time-mode=stream-time ypos=0 deltay=64 color=0xffffff00"
+TIME_OVERLAY1="identity"
+TIME_OVERLAY2="identity"
+
+#VIS_PLUGIN="julia_vis map-func=rational-interp"
+#VIS_PLUGIN="julia_vis map-func=normal"
+VIS_PLUGIN="julia_vis"
+
+
+VID_FORMAT="video/x-raw,width=512,height=512,framerate=60/1"
+VID_FORMAT="video/x-raw,width=1024,height=1024,framerate=60/1"
+#VID_FORMAT="video/x-raw,width=512,height=512,framerate=30/1"
+#VID_FORMAT="video/x-raw,format=RGB15,width=512,height=512,framerate=30/1"
+
+
+VID_SINK="fpsdisplaysink video-sink=xvimagesink"
+#VID_SINK="fpsdisplaysink"
+
+#GDB='valgrind'
+#VID_SINK="fakesink"
+#VID_FORMAT="video/x-raw,width=128,height=128,framerate=10/1"
+
+VID_FRAME_RATE=60
+VID_WIDTH=1024
+VID_HEIGHT=1024
+
+INPUT_AUDIO="test.mid"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -h|--help)
+      echo "Usage:\n"
+      #TODO
+      shift # past argument
+      ;;
+    -g|--gdb)
+      GDB='gdb --args'
+      shift # past argument
+      ;;
+    -v|--valgrind)
+      GDB='valgrind'
+      shift # past argument
+      ;;
+    -l|--log)
+      DEBUG_ARGS="--gst-debug=julia_vis:9 --gst-debug=baseaudiovisualizer-julia:9"
+      shift # past argument
+      ;;
+    -i|--input)
+      INPUT_AUDIO="$2"
+      shift # past argument
+      shift # past value
+      ;;
+
+    -r|--resolution)
+      VID_WIDTH=${2%%x}
+      VID_HEIGHT=${2#x}
+      shift # past argument
+      shift # past value
+      ;;
+    
+    --default)
+      DEFAULT=YES
+      shift # past argument
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+GST_PLUGIN_PATH=bin/.libs $GDB gst-launch-1.0 $DEBUG_ARGS \
+streamsynchronizer name=s \
+filesrc location=meco_star_wars_disco.mid ! decodebin ! audioconvert  ! rgvolume ! audioconvert ! tee name=t \
+t.src_0 ! queue ! $VIS_PLUGIN ! $VID_FORMAT ! $TIME_OVERLAY1 ! queue max-size-buffers=2 ! s.sink_0 \
+t.src_1 ! queue ! audioconvert ! audioresample ! s.sink_1 \
+s.src_0 !  $TIME_OVERLAY2 ! videoconvert ! $VID_SINK \
+s.src_1 ! autoaudiosink
+
+
